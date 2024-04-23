854f4b51c8aab11db4b558e06e92220de15b59b4
Iqrash Jalil <iqrashjalil@gmail.com>
2024-04-24 02:26:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connectDB();
 
 
 var corsOptions = {
-  origin:  'https://cheerful-froyo-b7f616.netlify.app',
+  origin:  ['https://cheerful-froyo-b7f616.netlify.app','https://powerballotfrontend.vercel.app/'],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true
 };
